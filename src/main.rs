dee819671e575a737f33c15e128ff7739509b7c9
Andreas Schröder <andschroeder@uos.de>
2017-01-25 19:24:56 +0100

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -7,65 +7,158 @@ use piston::window::WindowSettings;
 use piston::event_loop::*;
 use piston::input::*;
 use glutin_window::GlutinWindow as Window;
-use opengl_graphics::{ GlGraphics, OpenGL };
+use opengl_graphics::{GlGraphics, OpenGL};
+use graphics::*;
+
+const OPENGL: OpenGL = OpenGL::V3_2;
+const WIDTH: i64 = 800;
+const HEIGHT: i64 = 640;
+
+pub struct Creature {
+    x: f64,
+    y: f64,
+    rot: f64,
+    rect: [f64; 4],
+}
 
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
-    rotation: f64   // Rotation for the square.
+    up_d: bool,
+    down_d: bool,
+    left_d: bool,
+    right_d: bool,
+    player: Creature,
+}
+
+impl Creature {
+    fn new() -> Self {
+        Creature {
+            x: (WIDTH / 2) as f64,
+            y: (HEIGHT / 2) as f64,
+            rot: 0.0,
+            rect: rectangle::square(0.0, 0.0, 50.0),
+        }
+    }
+    fn moves(&mut self, dx: f64, dy: f64) {
+        self.x = border_add(self.x, dx, true);
+        self.y = border_add(self.y, dy, false);
+    }
 }
 
 impl App {
+    fn new() -> Self {
+        App {
+            gl: GlGraphics::new(OPENGL), // OpenGL drawing backend.
+            up_d: false,
+            down_d: false,
+            left_d: false,
+            right_d: false,
+            player: Creature::new(),
+        }
+    }
+
     fn render(&mut self, args: &RenderArgs) {
-        use graphics::*;
+
 
         const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-        const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+        const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+        const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+        const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
 
-        let square = rectangle::square(0.0, 0.0, 50.0);
-        let rotation = self.rotation;
-        let (x, y) = ((args.width / 2) as f64,
-                      (args.height / 2) as f64);
 
+        let square_length = 20;
+        let field_width = 640 / square_length - 2;
+        let field_heigth = 800 / square_length - 2;
+        let mut vec: Vec<[f64; 4]> = Vec::new();
+
+        for i in 0..field_heigth {
+            for j in 0..field_width {
+                vec.push(rectangle::square((20 + square_length * i) as f64,
+                                           (20 + square_length * j) as f64,
+                                           square_length as f64));
+            }
+        }
+        let (x, y) = (self.player.x as f64, self.player.y as f64);
+        let player = self.player.rect;
+                let rotation = self.player.rot;
         self.gl.draw(args.viewport(), |c, gl| {
             // Clear the screen.
             clear(GREEN, gl);
 
-            let transform = c.transform.trans(x, y)
-                                       .rot_rad(rotation)
-                                       .trans(-25.0, -25.0);
+            let transform = c.transform
+                .trans(x, y)
+                .rot_rad(rotation)
+                .trans(-25.0, -25.0);
 
             // Draw a box rotating around the middle of the screen.
-            rectangle(RED, square, transform, gl);
+            for i in 0..(field_heigth) {
+                for j in 0..field_width {
+                    rectangle(if i % 2 == j % 2 { BLACK } else { WHITE },
+                              vec[i * field_width + j],
+                              c.transform,
+                              gl);
+                }
+            }
+            
+            rectangle(RED, player, transform, gl);
         });
     }
 
     fn update(&mut self, args: &UpdateArgs) {
         // Rotate 2 radians per second.
-        self.rotation += 2.0 * args.dt;
+        self.player.rot += 2.0 * args.dt;
+        if self.up_d {
+            self.player.moves(0.0, -1.0);
+        }
+        if self.down_d {
+            self.player.moves(0.0, 1.0);
+        }
+        if self.right_d {
+            self.player.moves(1.0, 0.0);
+        }
+        if self.left_d {
+            self.player.moves(-1.0, 0.0);
+        }
+    }
+
+    fn on_input(&mut self, inp: Button, pressed: bool) {
+
+        match inp {
+            Button::Keyboard(Key::Up) => {
+                self.up_d = pressed;
+            }
+            Button::Keyboard(Key::Down) => {
+                self.down_d = pressed;
+            }
+            Button::Keyboard(Key::Left) => {
+                self.left_d = pressed;
+            }
+            Button::Keyboard(Key::Right) => {
+                self.right_d = pressed;
+            }
+            _ => {}
+
+        }
     }
 }
 
 fn main() {
     // Change this to OpenGL::V2_1 if not working.
-    let opengl = OpenGL::V3_2;
+
 
     // Create an Glutin window.
-    let mut window: Window = WindowSettings::new(
-            "spinning-square",
-            [200, 200]
-        )
-        .opengl(opengl)
+    let mut window: Window = WindowSettings::new("spinning-square", [800, 640])
+        .opengl(OPENGL)
         .exit_on_esc(true)
         .build()
         .unwrap();
 
     // Create a new game and run it.
-    let mut app = App {
-        gl: GlGraphics::new(opengl),
-        rotation: 0.0
-    };
+    let mut app = App::new();
 
     let mut events = window.events();
+
+
     while let Some(e) = events.next(&mut window) {
         if let Some(r) = e.render_args() {
             app.render(&r);
@@ -74,5 +167,27 @@ fn main() {
         if let Some(u) = e.update_args() {
             app.update(&u);
         }
+
+        if let Some(i) = e.press_args() {
+            app.on_input(i, true);
+        }
+        if let Some(i) = e.release_args() {
+            app.on_input(i, false);
+        }
+    }
+}
+
+//UTIL//////////////////////////////
+
+
+fn border_add(add1: f64, add2: f64, width: bool) -> f64 {
+    let border = (if width { WIDTH } else { HEIGHT }) as f64;
+    let sum = add1 + add2;
+    if sum < 0.0 {
+        0.0
+    } else if sum > border {
+        border
+    } else {
+        sum
     }
 }
